7706ec7960e6dd75872d918d8c7beff358b41d79
pedro.techuk <gutechuk@gmail.com>
2025-06-26 11:19:44 -0300

    feat: add plate filter

--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -413,9 +413,7 @@ layout('layouts.app');
                                 @break
                         @endswitch
                     </td>
-                    <td class="py-2 px-4 border-b text-center">
-                        {{ substr($multa->placa, 0, 3) . '-' . substr($multa->placa, 3) }}
-                    </td>
+                    <td class="py-2 px-4 border-b text-center">{{ $multa->placa }}</td>
                     <td class="py-2 px-4 border-b text-center relative group cursor-pointer">
                         <a href="{{ route('multas.info', ['id' => $multa->id]) }}">{{ $multa->auto_infracao }}</a>
                         <x-button icon="o-information-circle"
